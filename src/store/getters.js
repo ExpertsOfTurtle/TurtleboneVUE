ac787b460bf51ad966d9088c935a180458e0d882
fengrenchang86 <fengrenchang86@vip.qq.com>
2018-04-10 20:18:34 +0800

    delete all

--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  language: state => state.app.language,
-  visitedViews: state => state.app.visitedViews,
-  cachedViews: state => state.app.cachedViews,
-  permission_routers: state => state.permission.routers,
-  addRouters: state => state.permission.addRouters
-}
-export default getters
